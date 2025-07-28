7cc79a071b018c2c5edf636e54ae33022d606f66
Cyrille Herve <cyrillerv0103@gmail.com>
2025-07-28 15:58:08 +0100

    add the utilitary functions to compute the profit realized on each operation

--- backtesting/utils.py
+++ backtesting/utils.py
@@ -1,5 +1,6 @@
 # Fonctions auxiliaires (ex: to_cumprod, normalisation, etc.)
 import pandas as pd
+import numpy as np
 
 def compute_portfolio_pnl(
     portfolio_volume: pd.DataFrame,
@@ -105,3 +106,109 @@ def compute_top_contributors(
     top_best_contributors = contrib_ratio_best[contrib_ratio_best > 0.2]
 
     return top_worst_contributors, top_best_contributors
+
+
+# TODO: mettre les df en brut au début du fichier pour ne les écrire qu'une seule fois.
+
+def calculate_avg_buying_price(df_volume_flows, df_volume_en_portefeuille, df_stock_price) :
+    """
+    Crée le df qui affiche l'average buying (or selling) price pour chaque date où une opération est effectuée
+    """
+
+    # 1. On calcule les ABP qui ne nécessitent pas de passer par une boucle 
+    condition_open_position = (((df_volume_en_portefeuille.shift(1) == 0) | (pd.isna(df_volume_en_portefeuille.shift(1)))) & (df_volume_en_portefeuille != 0)) | (df_volume_en_portefeuille.shift(1) * df_volume_en_portefeuille < 0)
+    # ABP quand on ouvre une position
+    ABP_open_position = df_stock_price[condition_open_position].copy()
+
+
+    # Lorsqu'on renforce notre position après en avoir ouvert une juste avant
+    condition_renfo_after_open = ~condition_open_position & condition_open_position.shift(1) & (df_volume_flows * df_volume_en_portefeuille.shift(1) > 0)
+    ABP_renfo_after_open = ((df_volume_flows * df_stock_price + df_volume_en_portefeuille.shift(1) * df_stock_price.shift(1)) / df_volume_en_portefeuille)[condition_renfo_after_open].copy()
+
+
+    # 2. On filtre les lignes qui devront passer dans la boucle
+    # Condition qui filtre pour ne garder que les opérations qui réduisent la position en cours 
+    condition_reduction_pos = (df_volume_flows * df_volume_en_portefeuille.shift(1) < 0) & (df_volume_en_portefeuille * df_volume_en_portefeuille.shift(1) > 0)
+
+    # On ne garde que les lignes pour lesquelles on renfo la position mais que l'opération précédente n'était pas un open position.
+    # Le replace nan qu'on fait n'a pas d'importance car ça sera pour la première opération et elle n'est forcément pas concernée car on ne garde que les renfo pour lesquels pas d'open position juste avant = > minimum 2 opérations faites au préalable.
+    condition_renfo_last_not_open = (~condition_open_position.shift(1).replace(np.nan, False)) & (df_volume_en_portefeuille * df_volume_en_portefeuille.shift(1) > 0) & (df_volume_flows * df_volume_en_portefeuille.shift(1) > 0)
+
+
+    ABP_before_loop = ABP_open_position.fillna(0) + ABP_renfo_after_open.fillna(0)
+
+    dic_ABP = {}
+
+    for idx in df_volume_flows.index:
+        for stock in df_volume_flows.columns:
+
+            key = (idx, stock)
+
+            # Ancienne valeur de ABP
+            prev_key = (df_volume_flows.index[df_volume_flows.index.get_loc(idx) - 1], stock) \
+                if df_volume_en_portefeuille.index.get_loc(idx) > 0 else None
+
+            abp_prev = dic_ABP.get(prev_key, 0)
+
+            # Si on est sur une réduction de position, alors on ne change pas l'ABP => changer si on veut faire du FIFO.
+            if condition_reduction_pos.loc[idx, stock]:
+                dic_ABP[key] = abp_prev
+
+            # Si on renforce notre position et la transaction précédente n'était pas un open (donc un autre renfo), alors on fait ce calcul.
+            elif condition_renfo_last_not_open.loc[idx, stock]:
+                numerator = (
+                    df_volume_flows.loc[idx, stock] * df_stock_price.loc[idx, stock]
+                    + df_volume_en_portefeuille.loc[prev_key[0], stock] * abp_prev
+                )
+                denominator = df_volume_en_portefeuille.loc[idx, stock]
+                dic_ABP[key] = numerator / denominator if denominator != 0 else 0
+
+            # Si ce n'est aucun des ces deux cas là, alors on l'a forcément déjà calculé hors boucle.
+            else:
+                dic_ABP[key] = ABP_before_loop.loc[idx, stock]
+
+
+    # 1. Convertir le dictionnaire en DataFrame
+    df_ABP = pd.DataFrame.from_dict(dic_ABP, orient='index', columns=['ABP'])
+
+    # 2. Transformer l’index tuple en MultiIndex propre
+    df_ABP.index = pd.MultiIndex.from_tuples(df_ABP.index, names=["date", "stock"])
+
+    # 3. (Optionnel) Remettre en forme : un index = date, colonnes = stock
+    df_ABP = df_ABP.unstack("stock")  # chaque colonne devient ABP d’un stock
+
+    # 4. (Optionnel) Renommer colonnes
+    df_ABP.columns = df_ABP.columns.droplevel(0)  # retire le niveau "ABP" si inutile
+
+    # On vérifie qu'il n'y ait aucun buying price négatif
+    assert (df_ABP < 0).sum().sum() == 0, f"Problème dans les calculs de l'average buying price: Il y a des prix négatifs."
+    
+    return df_ABP
+
+def calculate_profit_transaction(df_volume_en_portefeuille, df_stock_price, df_ABP) : 
+    """
+    1. On met l'ABP au closing dates, que ce soit un total close ou un partial.
+    2. On calcule la variation entre l'ABP et le closing price.
+    3. On multiplie la variation par la position closed (1 pour long et -1 pour short)
+    """
+    ## 1.
+    # Indicateur pour les close
+    # Si la valeur est égale à 0 et que la valeur précédente n'était pas 0, alors onn a une clôture totale
+    indicateur_total_close = (df_volume_en_portefeuille.eq(0) & df_volume_en_portefeuille.shift(1).ne(0) & df_volume_en_portefeuille.shift(1).notna())
+    # Si la valeur est différente de 0 et est inférieure à la valeur précédente et qu'on garde la même position, alors on a une clôture partielle
+    # Si la position actuelle est non nulle ET que le volume en valeur abs est inférieur au volume de la veille en abs ET que le volume d'ajourd'hui est du même signe que hier
+    indicateur_partial_close = df_volume_en_portefeuille.ne(0) & df_volume_en_portefeuille.abs().lt(df_volume_en_portefeuille.shift(1).abs()) & (df_volume_en_portefeuille * df_volume_en_portefeuille.shift(1) > 0)
+
+    indicateur_close = indicateur_total_close + indicateur_partial_close
+    # Pour chaque close, on met l'ABP de l'open
+    # On fait shift(1) car l'ABP est calculée après le close pour la ligne en question
+    df_ABP_closing_dates = df_ABP.reindex(df_volume_en_portefeuille.index).replace(0, np.nan).ffill().shift(1)[indicateur_close].copy()
+
+    ## 2.
+    var_on_closing_date = df_stock_price[indicateur_close] / df_ABP_closing_dates - 1
+
+    # TODO: simplifier l'expression de ligne ci dessous
+    ## 3.
+    profit_per_transaction = var_on_closing_date * ((df_volume_en_portefeuille.shift(1)[indicateur_close] > 0).astype(int) - (df_volume_en_portefeuille.shift(1)[indicateur_close]<0).astype(int))
+
+    return profit_per_transaction
