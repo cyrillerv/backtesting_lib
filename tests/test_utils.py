832ef667aa699bc0cc2f8f75a9b5c5001e9cc987
Cyrille Herve <cyrillerv0103@gmail.com>
2025-07-28 15:58:26 +0100

    add testing functions for the functions that compute the profit per transaction

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -112,3 +112,135 @@ def test_compute_portfolio_pnl_basic():
     # Vérifications
     pd.testing.assert_series_equal(daily, expected_daily)
     pd.testing.assert_series_equal(cumulative, expected_cumulative)
+
+
+def test_fct_avg_buying_price() :
+
+    # Dates fictives pour l'index (tu peux adapter selon ton cas)
+    dates = pd.date_range(start='2024-01-01', periods=14, freq='D')
+
+    # volume_flows
+    df_volume_flows = pd.DataFrame({
+        'A': [0, 5, 2, -4, -3, 10, -15, 6, -2, -10, 11, -5, 9, -4],
+        'B': [0, 5, 2, -4, -3, 10, -15, 6, -2, -10, 11, -5, 9, -4]
+    }, index=dates)
+
+    # volume en portefeuille
+    df_volume_en_portefeuille = pd.DataFrame({
+        'A': [0, 5, 7, 3, 0, 10, -5, 1, -1, -11, 0, -5, 4, 0],
+        'B': [0, 5, 7, 3, 0, 10, -5, 1, -1, -11, 0, -5, 4, 0]
+    }, index=dates)
+
+    # stockPrices
+    df_stock_price = pd.DataFrame({
+        'A': [100, 101, 105, 104, 90, 92, 91, 92, 92, 101, 110, 112, 111, 109],
+        'B': [100, 101, 105, 104, 90, 92, 91, 92, 92, 101, 110, 112, 111, 109]
+    }, index=dates)
+
+
+    # Création du DataFrame
+    df_ABP_check = pd.DataFrame({
+        'A': [
+            0.000000, 101.000000, 102.142857, 102.142857, 0.000000,
+            92.000000, 91.000000, 92.000000, 92.000000, 100.181818,
+            0.000000, 112.000000, 111.000000, 0.000000
+        ],
+        'B': [
+            0.000000, 101.000000, 102.142857, 102.142857, 0.000000,
+            92.000000, 91.000000, 92.000000, 92.000000, 100.181818,
+            0.000000, 112.000000, 111.000000, 0.000000
+        ]
+    }, index=pd.to_datetime([
+        '2024-01-01', '2024-01-02', '2024-01-03', '2024-01-04',
+        '2024-01-05', '2024-01-06', '2024-01-07', '2024-01-08',
+        '2024-01-09', '2024-01-10', '2024-01-11', '2024-01-12',
+        '2024-01-13', '2024-01-14'
+    ]))
+
+    df_ABP_check.index.name = 'date'
+    df_ABP_check.columns.name = 'stock'
+
+
+    res = calculate_avg_buying_price(df_volume_flows, df_volume_en_portefeuille, df_stock_price)
+
+    # Harmonise les noms de colonnes/index pour une comparaison stricte
+    res.columns.name = 'stock'
+    res.index.name = 'date'
+
+    # Vérifie l’égalité
+    assert (res.round(5) == df_ABP_check.round(5)).all().all()
+
+
+
+def test_fct_calc_profit_transaction() :
+
+    # Dates fictives pour l'index (tu peux adapter selon ton cas)
+    dates = pd.date_range(start='2024-01-01', periods=14, freq='D')
+
+    # volume en portefeuille
+    df_volume_en_portefeuille = pd.DataFrame({
+        'A': [0, 5, 7, 3, 0, 10, -5, 1, -1, -11, 0, -5, 4, 0],
+        'B': [0, 5, 7, 3, 0, 10, -5, 1, -1, -11, 0, -5, 4, 0]
+    }, index=dates)
+
+    # stockPrices
+    df_stock_price = pd.DataFrame({
+        'A': [100, 101, 105, 104, 90, 92, 91, 92, 92, 101, 110, 112, 111, 109],
+        'B': [100, 101, 105, 104, 90, 92, 91, 92, 92, 101, 110, 112, 111, 109]
+    }, index=dates)
+
+
+    # Création du DataFrame
+    df_ABP = pd.DataFrame({
+        'A': [
+            0.000000, 101.000000, 102.142857, 102.142857, 0.000000,
+            92.000000, 91.000000, 92.000000, 92.000000, 100.181818,
+            0.000000, 112.000000, 111.000000, 0.000000
+        ],
+        'B': [
+            0.000000, 101.000000, 102.142857, 102.142857, 0.000000,
+            92.000000, 91.000000, 92.000000, 92.000000, 100.181818,
+            0.000000, 112.000000, 111.000000, 0.000000
+        ]
+    }, index=pd.to_datetime([
+        '2024-01-01', '2024-01-02', '2024-01-03', '2024-01-04',
+        '2024-01-05', '2024-01-06', '2024-01-07', '2024-01-08',
+        '2024-01-09', '2024-01-10', '2024-01-11', '2024-01-12',
+        '2024-01-13', '2024-01-14'
+    ]))
+
+    df_ABP.index.name = 'date'
+    df_ABP.columns.name = 'stock'
+
+
+    df_profit_check = pd.DataFrame({
+        'A': [
+            0, 0, 0, 0.018182, -0.118881,
+            0,0,0,0,0,
+            -0.098004, 0,0, -0.018018
+        ],
+        'B': [
+            0, 0, 0, 0.018182, -0.118881,
+            0,0,0,0,0,
+            -0.098004, 0,0, -0.018018
+        ],
+    }, index=pd.to_datetime([
+        '2024-01-01', '2024-01-02', '2024-01-03', '2024-01-04',
+        '2024-01-05', '2024-01-06', '2024-01-07', '2024-01-08',
+        '2024-01-09', '2024-01-10', '2024-01-11', '2024-01-12',
+        '2024-01-13', '2024-01-14'
+    ]))
+
+    df_profit_check.index.name = 'date'
+    df_profit_check.columns.name = 'stock'
+
+
+    res = calculate_profit_transaction(df_volume_en_portefeuille, df_stock_price, df_ABP)
+    res.fillna(0, inplace=True)
+
+    # Harmonise les noms de colonnes/index pour une comparaison stricte
+    res.columns.name = 'stock'
+    res.index.name = 'date'
+
+    # Vérifie l’égalité
+    assert (res.round(5) == df_profit_check.round(5)).all().all()
