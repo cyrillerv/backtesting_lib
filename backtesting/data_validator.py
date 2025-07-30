6893205863d6e4a5c01259d69fd5fbda35cc4660
Cyrille Herve <cyrillerv0103@gmail.com>
2025-07-30 09:51:18 +0100

    add validation checking for df_benchmarks

--- backtesting/data_validator.py
+++ backtesting/data_validator.py
@@ -3,12 +3,12 @@ import numpy as np
 import warnings
 
 class DataValidator:
-    def __init__(self, orders_df, stock_prices_df, stop_loss_input, take_profit_input, bench_df_input=None):
-        self.orders_df = orders_df.copy()
-        self.stock_prices_df = stock_prices_df.copy()
+    def __init__(self, orders_df_input, stock_prices_df_input, stop_loss_input, take_profit_input, bench_df_input=None):
+        self.orders_df = orders_df_input.copy()
+        self.stock_prices_df = stock_prices_df_input.copy()
         self.stop_loss = stop_loss_input
         self.take_profit = take_profit_input
-        self.bench_df_input = bench_df_input.copy()
+        self.bench_df = bench_df_input.copy()
 
     def check_columns_orders(self) :
         required_columns = {"Date", "Symbol", "Volume", "Type"}
@@ -83,6 +83,33 @@ class DataValidator:
 
         print("Stock prices dataframe check passed.")
         return self.stock_prices_df
+    
+    def check_df_bench(self) :
+        # Check index type
+        assert isinstance(self.bench_df.index, pd.DatetimeIndex), "L'index n'est pas un DatetimeIndex"
+
+        # Check all columns are float dtype
+        non_float_cols = [col for col, dtype in self.bench_df.dtypes.items() if not pd.api.types.is_float_dtype(dtype)]
+        assert not non_float_cols, f"Ces colonnes ne sont pas de type float : {non_float_cols}"
+
+        # Check columns all NaN
+        cols_all_nan = self.bench_df.columns[self.bench_df.isna().all()]
+        if not cols_all_nan.empty:
+            raise ValueError(f"Les colonnes suivantes ne contiennent que des NaN : {list(cols_all_nan)}")
+
+        # Replace inf values and warn
+        cols_with_inf = self.bench_df.columns[np.isinf(self.bench_df).any()]
+        if not cols_with_inf.empty:
+            self.bench_df.replace([np.inf, -np.inf], np.nan, inplace=True)
+            warnings.warn(f"Les colonnes suivantes contiennent des valeurs infinies : {list(cols_with_inf)}. Remplacées par NaN puis ffill.", stacklevel=2)
+            self.bench_df.ffill(inplace=True)
+
+        # Fill remaining NaN
+        self.bench_df.ffill(inplace=True)
+        self.bench_df.bfill(inplace=True)
+
+        print("Stock prices dataframe check passed.")
+        return self.bench_df
 
     def check_consistency(self):
         orders_symbols = set(self.orders_df["Symbol"].to_list())
@@ -99,16 +126,25 @@ class DataValidator:
         self.stock_prices_df = self.stock_prices_df.loc[:, list(common_tickers)]
 
         start_date = self.orders_df['Date'].min()
-        end_date = self.orders_df['Date'].max()
+        last_order_date = self.orders_df['Date'].max()
+        end_date = self.stock_prices_df.index.max()
         full_date_range = pd.date_range(start=start_date, end=end_date, freq='D')
+
+        assert end_date >= last_order_date, f"Le df des stock prices ne va que jusqu'au {end_date} tandis que le dernier ordre est passé le {last_order_date}."
         self.stock_prices_df = self.stock_prices_df.reindex(full_date_range)
         self.stock_prices_df.ffill(inplace=True)
+        if not self.bench_df.empty :
+            assert self.bench_df.index.max() >= last_order_date, f"Le df des benchmarks ne va que jusqu'au {self.bench_df.index.max()} tandis que le dernier ordre est passé le {last_order_date}."
+            self.bench_df = self.bench_df.reindex(full_date_range)
+            self.bench_df.ffill(inplace=True)
 
         print("Consistency check passed.")
-        return self.orders_df, self.stock_prices_df, self.bench_df_input
+        return self.orders_df, self.stock_prices_df, self.bench_df
 
     def validate_all(self):
         self.check_columns_orders()
         self.check_columns_content()
         self.check_stock_prices()
+        if not self.bench_df.empty :
+            self.check_df_bench()
         return self.check_consistency()
